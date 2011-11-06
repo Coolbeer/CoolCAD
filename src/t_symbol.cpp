d3c39718cbb0a5bb03b99e089298288a10120c1c
Kolbjørn Fredheim <hightower@tregt.net>
2011-11-06 05:55:12 +0100

    t_symbol: Implement addLine function

--- src/t_symbol.cpp
+++ src/t_symbol.cpp
@@ -1,1 +1,6 @@
 #include "t_symbol.h"
+
+void t_symbol::addLine(QLine &line)
+{
+	internalLines.push_back(line);
+}
--- src/t_symbol.h
+++ src/t_symbol.h
@@ -1,5 +1,16 @@
 #ifndef T_SYMBOL_H
 #define T_SYMBOL_H
 
+#include <vector>
+#include <QtCore/QLine>
+
+class t_symbol
+{
+	public:
+		void					addLine(QLine &line);
+
+	private:
+		std::vector<QLine>		internalLines;
+};
 
 #endif
